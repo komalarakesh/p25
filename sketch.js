1d8328f1c36085ce6c6fe595a54cec6c39515ff6
komalarakesh <94545220+komalarakesh@users.noreply.github.com>
2022-01-24 17:49:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball,groundObj,leftSide,rightSide;
+var world;
+var radius = 70;
+
+var btn1;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		density:0.4
+	}
+	
+	
+	btn2 = createImg('dustbin.png');
+	btn2.position(1100,325,10,120);
+
+	ball = createImg('paper.png');
+	ball.position(100,492,20,120);
+
+	ground=new Ground(width/2,670,width,20);
+	btn2 = new Ground(1100,600,10,120);
+	rightSide = new Ground(1270,600,10,120);
+	bottomSide = new Ground(1185,650,150,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	background(200);
+	rectMode(CENTER);
+
+
+	ground.display();
+	leftSide.display();  
+	rightSide.display();
+	bottomSide.display();
+
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+    
+  	}
+}
